ca733d520d5ae9e8716d5fa2fcaa6829409901c0
Dennis Prins <dennis@wedevelop.nl>
2025-12-11 09:49:49 +0100

    Grid DB

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,11 @@
 namespace WeDevelop\StatamicGrid;
 
 use Statamic\Providers\AddonServiceProvider;
+use WeDevelop\StatamicGrid\Commands\MigrateGridData;
+use WeDevelop\StatamicGrid\Elements\ImageElement;
+use WeDevelop\StatamicGrid\Elements\TextElement;
+use WeDevelop\StatamicGrid\Fieldtypes\Grid;
+use WeDevelop\StatamicGrid\Registries\ElementRegistry;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -10,12 +15,35 @@ class ServiceProvider extends AddonServiceProvider
         Tags\Grid::class,
     ];
 
+    protected $fieldtypes = [
+        Grid::class,
+    ];
+
+    protected $commands = [
+        MigrateGridData::class,
+    ];
+
     public function bootAddon()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'statamic-grid');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'statamic-grid');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->publishes([
-            __DIR__.'/../resources/fieldsets' => resource_path('fieldsets/vendor/statamic-grid'),
-        ], 'statamic-grid');
+        $this->registerElements();
+    }
+
+    /**
+     * Register all element types.
+     *
+     * Projects can register additional elements in their AppServiceProvider:
+     *
+     *     use WeDevelop\StatamicGrid\Registries\ElementRegistry;
+     *     use App\Elements\VideoElement;
+     *
+     *     ElementRegistry::register(VideoElement::class);
+     */
+    protected function registerElements(): void
+    {
+        ElementRegistry::register(TextElement::class);
+        ElementRegistry::register(ImageElement::class);
     }
 }
